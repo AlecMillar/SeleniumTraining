534189d07c3e68cd82283eda8e0c886b3a99f1e9
Alec Millar <alecrm94@vt.edu>
2017-07-13 19:46:15 -0400

    quick update to AmazonSearch app

--- src/com/SDETtraining/amazon/AmazonSearch.java
+++ src/com/SDETtraining/amazon/AmazonSearch.java
@@ -10,12 +10,33 @@ import org.testng.annotations.Test;
 
 import utilities.DriverFactory;
 
+/*
+ * This app will run a test search on Amazon for
+ * four items (read from Excel)and add them to your cart.
+ * It will then check the cart for the sub-total
+ * cost of the order and display this value.
+ * 
+ */
+
+/*
+ * EXEL ENTREES USED FOR TEST (no quotes):
+ *   	  A							 B
+ * 	1	"aps"					"trash can"
+ * 	2	"aps"					"electronics"
+ * 	3	"electronics"			"iPhone Charger"
+ * 	4	"local-services"		"backpack"
+ */
+
 public class AmazonSearch {
 
 	WebDriver driver;
 	String baseUrl = "https://www.amazon.com/";
-	private static String email = "pmillar@evergreen.org";
-	private static String password = "godloves";
+	
+	/*
+	 * Must insert email and password to use
+	 */
+	private static String email = null;
+	private static String password = null;
 
 	// Test to search for items on amazon & add them to the cart.
 	@Test (dataProvider = "getSearchData")
@@ -31,10 +52,12 @@ public class AmazonSearch {
 		// Sends requested item to cart
 		driver.findElement(By.xpath("html/body/div[1]/div[2]/div/div[3]/div[2]/div/div[4]/div[1]/div/ul/li[6]/div/div/div/div[2]/div[1]/div[1]/a/h2")).click();
 		driver.findElement(By.id("add-to-cart-button")).submit();
+		
 		// Tests to make sure item was added to cart
 		smoketests.AmazonAddCheck.testAddToCart(driver, "Added to Cart", searchObject);
 	}
 
+	// Instantiates driver, navigates to Amazon, and signs in.
 	@BeforeClass
 	public void beforeClass() {
 		new utilities.DriverFactory();
@@ -47,6 +70,7 @@ public class AmazonSearch {
 		driver.findElement(By.id("ap_password")).sendKeys(password);
 		driver.findElement(By.id("signInSubmit")).click();
 	}
+	// Checks cart, retrieves order sub-total, and quits of browser.
 	@AfterClass
 	public void afterClass() {
 		driver.findElement(By.id("nav-cart")).click();
@@ -59,6 +83,7 @@ public class AmazonSearch {
 		driver.quit();
 	}
 
+	// Retrieves data from Excel sheet
 	@DataProvider
 	public String[][] getSearchData() {
 		return datadriven.Excel.get("C:/Users/Alec/Google Drive/Work/SDET Training/Test Data/amazonsearch.xls");
